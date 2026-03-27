0509cceeaccb50893d91585b461f618c513214f1
Calum Erskine <calum.erskine@live.co.uk>
2026-03-27 13:18:29 +0100

    feat: add haptic feedback to Switch component

--- app/components/ui/switch.tsx
+++ app/components/ui/switch.tsx
@@ -1,15 +1,27 @@
-import { forwardRef } from "react";
+import { forwardRef, useCallback } from "react";
 import { Switch as RNSwitch, SwitchProps } from "react-native";
+import { useHaptics, type HapticPreset } from "@/hooks/use-haptics";
 
 type Props = SwitchProps & {
   value: boolean;
   onChange: (value: boolean) => void;
+  hapticPreset?: "selection" | "impactLight";
 };
 export default forwardRef(function Switch(
-  { value, onChange, ...props }: Props,
+  { value, onChange, hapticPreset = "selection", ...props }: Props,
   ref: React.Ref<RNSwitch>
 ) {
+  const { trigger } = useHaptics();
+
+  const handleValueChange = useCallback(
+    (next: boolean) => {
+      trigger(hapticPreset as HapticPreset);
+      onChange(next);
+    },
+    [trigger, hapticPreset, onChange],
+  );
+
   return (
-    <RNSwitch {...props} value={value} onValueChange={onChange} ref={ref} />
+    <RNSwitch {...props} value={value} onValueChange={handleValueChange} ref={ref} />
   );
 });
